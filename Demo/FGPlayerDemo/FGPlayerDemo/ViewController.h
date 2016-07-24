5953eb7b9847fb130e81054466fdee61a396bce0
chaowei <geekw@foxmail.com>
2016-07-24 14:38:25 +0800

    initial commit

---
+++ Demo/FGPlayerDemo/FGPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FGPlayerDemo
+//
+//  Created by weichao on 16/7/24.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/FGPlayerDemo/FGPlayerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FGPlayerDemo
+//
+//  Created by weichao on 16/7/24.
+//  Copyright © 2016年 chaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
